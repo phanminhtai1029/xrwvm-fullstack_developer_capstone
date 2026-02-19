3bc2e75dc521772bf5417543ac28fe1247476ce1
kource <phanminhtai1029@gmail.com>
2026-02-19 11:52:25 +0700

    Add Django proxy services: restapis.py, get_dealerships, get_dealer_details, get_dealer_reviews, add_review views; add analyzereview output

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -17,4 +17,17 @@ urlpatterns = [
     # path for get_cars
     path(route='get_cars', view=views.get_cars, name='getcars'),
 
+    # path for get_dealerships
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+
+    # path for get_dealer_details
+    path(route='get_dealer/<int:dealer_id>', view=views.get_dealer_details, name='get_dealer_details'),
+
+    # path for get_dealer_reviews
+    path(route='get_reviews/<int:dealer_id>', view=views.get_dealer_reviews, name='get_dealer_reviews'),
+
+    # path for add_review
+    path(route='add_review', view=views.add_review, name='add_review'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
